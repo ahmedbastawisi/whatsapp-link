d0f3f5599371fbd2ac914d947c2ea32332920408
ahmedbastawisi <ahmedbastawisi@outlook.com>
2023-07-31 21:51:36 +0300

    minor cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import './App.css'
 import WhatsAppLogo from './assets/WhatsAppButtonWhiteLarge.svg'
-import { FluentProvider, webLightTheme, Button, Input, Label, Image} from "@fluentui/react-components";
+import {
+  FluentProvider,
+  webLightTheme,
+  Button,
+  Input,
+  Label,
+  Textarea,
+  Image
+} from "@fluentui/react-components";
 
 export default function App() {
-      return (
-          <FluentProvider theme={webLightTheme}>
-            <Image src={WhatsAppLogo} alt='WhatsApp logo'></Image>
-            <Label>https://wa.me/</Label>
-            <Input placeholder='1XXXXXXXXXX'></Input>
-              <Button appearance="primary"> Chat</Button>
-          </FluentProvider>
-      );
+  return (
+    <FluentProvider theme={webLightTheme}>
+      <div className='content'>
+        <Image src={WhatsAppLogo} alt='WhatsApp logo'></Image>
+        <Label>https://wa.me/</Label>
+        <Input type='tel' placeholder='1XXXXXXXXXX'></Input>
+        <Textarea placeholder='message'></Textarea>
+        <Button appearance="primary">Chat</Button>
+      </div>
+    </FluentProvider>
+  );
 }
